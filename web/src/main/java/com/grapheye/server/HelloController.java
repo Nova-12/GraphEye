943cdccfc81cb88ba27561cd803074cffcaeb267
blukat29 <yunjong@kaist.ac.kr>
2016-04-04 21:26:56 +0900

    [web] Add basic api /api/user

---
+++ web/src/main/java/com/grapheye/server/HelloController.java
@@ -0,0 +1,29 @@
+package com.grapheye.server;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController
+{
+    private static final Logger logger =
+        Logger.getLogger(HelloController.class);
+
+    @RequestMapping(value="/user/{userid}", method=RequestMethod.GET)
+    public ModelAndView user(@PathVariable(value="userid") String id)
+    {
+        logger.info("In user()");
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("user");
+        model.addObject("greeting", "Hello user " + id);
+        return model;
+    }
+}
+
---
+++ web/src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head>
+  <body>
+    User info: ${ greeting }
+  </body>
+</html>
