4b7c71ab2bcba7d5799326629800fa67ca8ab5bd
blukat29 <yunjong@kaist.ac.kr>
2016-04-09 13:46:57 +0900

    Enforce gitignore inside sencha_env

--- sencha_env/build/temp/development/grapheye/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/jaewook/GraphEye/sencha_env/build/.sass-cache'
--- sencha_env/build/temp/development/grapheye/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/jaewook/GraphEye/sencha_env/app.js
