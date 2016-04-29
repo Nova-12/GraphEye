80a8418131f571f6d1cf6771b21328a10506407f
blukat29 <yunjong@kaist.ac.kr>
2016-04-29 17:01:29 +0900

    Use scallop to parse command line arguments

--- core/src/main/scala/com/grapheye/core/App.scala
+++ core/src/main/scala/com/grapheye/core/App.scala
@@ -2,52 +2,64 @@ package com.grapheye.core
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx.GraphLoader
 import org.apache.spark.graphx.VertexId
-import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.VertexRDD
+import org.apache.spark.rdd.RDD
+
+import org.rogach.scallop.ScallopConf
+import org.rogach.scallop.exceptions.RequiredOptionNotFound
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+  banner("Usage: ./run.sh output-dest input-type input-source\n\nOptions:")
+  val output = trailArg[String]()
+  val edgeFile = trailArg[String]()
+  val nodeFile = trailArg[String](required=false)
+  override def onError(e: Throwable): Unit = {
+    printHelp()
+    System.exit(1)
+  }
+  verify()
+}
 
 object App {
   def main(args: Array[String]) {
 
-    if (args.length < 2) {
-      System.out.println("Boom!");
-      System.exit(1);
+    /* Load and parse configurations. */
+    val conf = new Conf(args)
+
+    val sconf = new SparkConf();
+    val sc = new SparkContext(sconf)
+
+    /* Import */
+    System.out.println("Importing..")
+    val graph = GraphLoader.edgeListFile(sc, conf.edgeFile())
+
+    var nodeVertices: RDD[(VertexId, String)] = null;
+    if (conf.nodeFile.isSupplied) {
+      val nodeList = sc.textFile(conf.nodeFile())
+      nodeVertices = nodeList.map(
+        v => (v.split(" ")(0).toLong, v.split(" ")(1))
+      )
     }
 
-    val edgeListFilePath = args(1);
-    var nodeListFilePath:String = null;
-    var nodeList: RDD[String] = null;
-    var vertexRDD: RDD[(VertexId, String)] = null;
-    var results: Graph[(String, Double), Double] = null;
+    /* Compute */
+    System.out.println("Computing..")
+    val ranks = graph.pageRank(0.0001)
 
-    if (args.length > 2){
-      nodeListFilePath = args(2);
-    }
-    System.out.println("Hello!");
-
-    val conf = new SparkConf();
-    val sc = new SparkContext(conf);
-
-    val graph = GraphLoader.edgeListFile(sc, edgeListFilePath);
-    val ranks = graph.pageRank(0.0001);
-
-    if (nodeListFilePath != null){
-      nodeList = sc.textFile(nodeListFilePath);
-      vertexRDD = nodeList.map(v => (v.split(" ")(0).toLong, v.split(" ")(1)));
-      results = ranks.outerJoinVertices(vertexRDD){
-        (v, rank, title) => (title.getOrElse(" "), rank);
+    /* Export */
+    System.out.println("Exporting..")
+    val exporter = new Exporter("localhost:27017", "test", conf.output())
+    if (nodeVertices != null){
+      val ranksWithNode = ranks.outerJoinVertices(nodeVertices) {
+        (v, rank, title) => (title.getOrElse(" "), rank)
       }
-    }
-
-    val exporter = new Exporter("localhost:27017", "test", args(0))
-
-    if (results != null){
-      exporter.exportPageRankWithNode(results.vertices)
+      exporter.exportPageRankWithNode(ranksWithNode.vertices)
     }
     else{
       exporter.exportPageRank(ranks.vertices)
     }
-    System.out.println("Bye!");
+
+    System.out.println("Done!")
   }
 }
