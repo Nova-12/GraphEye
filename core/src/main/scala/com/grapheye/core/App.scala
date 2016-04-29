127a2eed8e345387b7b80bcb1b1e1c7dbeb17901
blukat29 <yunjong@kaist.ac.kr>
2016-04-29 15:46:40 +0900

    Separate exporter to Exporter.scala

--- core/src/main/scala/com/grapheye/core/App.scala
+++ core/src/main/scala/com/grapheye/core/App.scala
@@ -7,11 +7,6 @@ import org.apache.spark.graphx.GraphLoader
 import org.apache.spark.graphx.VertexId
 import org.apache.spark.graphx.Graph
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-
-import org.bson.Document;
-
 object App {
   def main(args: Array[String]) {
 
@@ -19,9 +14,6 @@ object App {
       System.out.println("Boom!");
       System.exit(1);
     }
-    var client = new MongoClient("localhost:27017");
-    var db = client.getDatabase("test");
-    var collection = db.getCollection(args(0));
 
     val edgeListFilePath = args(1);
     var nodeListFilePath:String = null;
@@ -48,37 +40,14 @@ object App {
       }
     }
 
-    var document = new Document();
- 
+    val exporter = new Exporter("localhost:27017", "test", args(0))
+
     if (results != null){
-      results.vertices.top(100){
-        Ordering.by((entry: (VertexId, (String, Double))) => entry._2._2)
-      }.foreach(
-        v => makeObjectWithNode(v, collection)
-      );
+      exporter.exportPageRankWithNode(results.vertices)
     }
     else{
-      ranks.vertices.top(100)(
-        Ordering.by(_._2)
-      ).foreach(
-        v => makeObject(v, collection)
-      );
+      exporter.exportPageRank(ranks.vertices)
     }
     System.out.println("Bye!");
-  }
-
-  def makeObjectWithNode(v: (VertexId, (String, Double)), collection: MongoCollection[Document])
-  {
-    var document = new Document();
-    document.append("node", v._2._1)
-    document.append("rank", v._2._2)
-    collection.insertOne(document)
-  }
-  def makeObject(v: (VertexId, Double), collection: MongoCollection[Document])
-  {
-    var document = new Document();
-    document.append("node", v._1)
-    document.append("rank", v._2)
-    collection.insertOne(document)
   }
 }
---
+++ core/src/main/scala/com/grapheye/core/Exporter.scala
@@ -0,0 +1,35 @@
+package com.grapheye.core
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import org.apache.spark.graphx.VertexId
+import org.apache.spark.graphx.VertexRDD;
+import org.bson.Document;
+
+class Exporter(mongoAddress: String, dbName: String, collectionName: String) {
+  val client = new MongoClient(mongoAddress)
+  val db = client.getDatabase(dbName)
+  val collection = db.getCollection(collectionName)
+
+  def insertPageRankEntry(node: String, rank: Double) {
+    var document = new Document()
+    document.append("node", node)
+    document.append("rank", rank)
+    collection.insertOne(document)
+  }
+  def exportPageRank(result: VertexRDD[Double]) {
+    result.top(100) {
+      Ordering.by(_._2)
+    }.foreach(
+      (v: (VertexId, Double)) => insertPageRankEntry(v._1.toString, v._2)
+    )
+  }
+  def exportPageRankWithNode(result: VertexRDD[(String, Double)]) {
+    result.top(100) {
+      Ordering.by(_._2._2)
+    }.foreach(
+      (v: (VertexId, (String, Double))) => insertPageRankEntry(v._2._1, v._2._2)
+    )
+  }
+}
+
