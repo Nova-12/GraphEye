d95f13efe996856a7dcd9c60aa19ae1d7041a85d
blukat29 <yunjong@kaist.ac.kr>
2016-06-07 19:15:29 +0900

    Implement mongodb importer in core

--- core/src/main/scala/com/grapheye/core/Importer.scala
+++ core/src/main/scala/com/grapheye/core/Importer.scala
@@ -1,9 +1,14 @@
 package com.grapheye.core
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.spark.SparkContext
+import org.apache.spark.graphx.Edge
 import org.apache.spark.graphx.Graph
 import org.apache.spark.graphx.GraphLoader
 import org.apache.spark.graphx.VertexId
+import org.bson.BSONObject
+import org.bson.BasicBSONObject
+import org.bson.types.ObjectId
 
 class Importer(sc: SparkContext, conf: Conf) {
 
@@ -30,7 +35,40 @@ class Importer(sc: SparkContext, conf: Conf) {
   }
 
   def mongodb(): Graph[String, Int] = {
-    return textFile()
+
+    val edgeConf = new Configuration()
+    edgeConf.set("mongo.input.uri", conf.edge())
+
+    val mongoEdgeRDD = sc.newAPIHadoopRDD(edgeConf,
+      classOf[com.mongodb.hadoop.MongoInputFormat],
+      classOf[Object],
+      classOf[BSONObject])
+
+    val edges = mongoEdgeRDD.map(
+      (v: (Object, BSONObject)) => {
+        var from = v._2.get("from").asInstanceOf[Int].asInstanceOf[Long]
+        var to = v._2.get("to").asInstanceOf[Int].asInstanceOf[Long]
+        Edge[Int](from, to, 0)
+      }
+    )
+
+    val nodeConf = new Configuration()
+    nodeConf.set("mongo.input.uri", conf.node())
+
+    val mongoNodeRDD = sc.newAPIHadoopRDD(nodeConf,
+      classOf[com.mongodb.hadoop.MongoInputFormat],
+      classOf[Object],
+      classOf[BSONObject])
+
+    val nodes = mongoNodeRDD.map(
+      (v: (Object, BSONObject)) => {
+        var id = v._2.get("id").asInstanceOf[Int].asInstanceOf[Long]
+        var name = v._2.get("name").asInstanceOf[String]
+        (id, name)
+      }
+    )
+
+    return Graph(nodes, edges)
   }
 
   def importGraph(): Graph[String, Int] = {
